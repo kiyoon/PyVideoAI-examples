8e4541d29ef2f5a708aa581a5f824bb8af83d4fa
Kiyoon Kim <yoonkr33@gmail.com>
2021-08-06 10:28:28 -0400

    wrong code fixed

--- 00-storage_location.py
+++ 00-storage_location.py
@@ -1,2 +1,6 @@
+# During the tutorial, we will create
+# $HDD_PATH/datasets, $HDD_PATH/pretrained, $HDD_PATH/PyVideoAI_experiments,
+# and $SSD_PATH/datasets.
+
 HDD_PATH = '/storage/kiyoon'
 SSD_PATH = '/faster/kiyoon'
